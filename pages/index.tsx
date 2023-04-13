c660e2d713895a8236882180a9fb69c2adeb5e9c
Mayank Singh <msfunbook@gmail.com>
2023-04-13 12:59:26 +0000

    Build project

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import WalletContextProvider from '../components/WalletContextProvider'
 import { AppBar } from '../components/AppBar'
-import { BalanceDisplay } from '../components/BalanceDisplay'
+// import { BalanceDisplay } from '../components/BalanceDisplay'
 import { PingButton } from '../components/PingButton'
 import Head from 'next/head'
 

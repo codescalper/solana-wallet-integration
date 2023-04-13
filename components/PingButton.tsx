3d795ae25b81329745e6bce1c4ea31b748b22ad0
Mayank Singh <msfunbook@gmail.com>
2023-04-13 12:48:02 +0000

    Ping Button fucntion

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,36 +1,37 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import * as web3 from '@solana/web3.js'
+import * as Web3 from '@solana/web3.js'
 import { FC } from 'react'
 import styles from '../styles/PingButton.module.css'
 
-const PROGRAM_ID = `ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa`
-const DATA_ACCOUNT_PUBKEY = `Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod`
+const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
 
 export const PingButton: FC = () => {
 	const { connection } = useConnection();
 	const { publicKey, sendTransaction } = useWallet();
 
 	const onClick = () => {
-		if (!connection || !publicKey) { return }
+		if (!connection || !publicKey) { 
+			alert("Please connect your wallet first lol")
+			return
+		}
 
-		const programId = new web3.PublicKey(PROGRAM_ID)
-		const programDataAccount = new web3.PublicKey(DATA_ACCOUNT_PUBKEY)
-		const transaction = new web3.Transaction()
+		const transaction = new Web3.Transaction()
 
-		const instruction = new web3.TransactionInstruction({
+		const instruction = new Web3.TransactionInstruction({
 			keys: [
 				{
-					pubkey: programDataAccount,
+					pubkey: PROGRAM_DATA_PUBLIC_KEY,
 					isSigner: false,
 					isWritable: true
 				},
 			],
-			programId
+			programId: PROGRAM_ID,
 		});
 
 		transaction.add(instruction)
 		sendTransaction(transaction, connection).then(sig => {
-			console.log(sig)
+			console.log(`Explorer URL: https://explorer.solana.com/tx/${sig}?cluster=devnet`)
 		})
 	}
 
